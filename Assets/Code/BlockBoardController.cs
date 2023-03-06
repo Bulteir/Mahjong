3e5a44057cb98a22f6e259100b614be756010e08
bulteir <talha_alpay20@hotmail.com>
2023-03-06 13:51:48 +0300

    -temel oyun mekaniği tamamlandı. -bloklar ıstaka yerleştirilince güzel şekilde kayıyor. -3lü eşleme olunca patlıyor. -aynı tipteki taşlar yan yana geliyor.

--- Assets/Code/BlockBoardController.cs
+++ Assets/Code/BlockBoardController.cs
@@ -17,20 +17,85 @@ public class BlockBoardController : MonoBehaviour
 
     List<BlockSlotStruct> blockSlotsStruckList = new List<BlockSlotStruct>();
     public List<Transform> BlockSlots = new List<Transform>();
+    bool isSmoothMoveToSnapPointAnimationContinue = false;
 
-    // Start is called before the first frame update
-    void Start()
+    public void PlaceBlock(Transform SelectedBlock)
     {
-
+        if (isSmoothMoveToSnapPointAnimationContinue == false)
+        {
+            //ýstakanýn durumunu alýyoruz.
+            FillBlockBoardStatus();
+            int avaibleSlotIndex = GetAvaibleBlockSlotIndex(SelectedBlock);
+            if (avaibleSlotIndex != -1) //bloklarý ýstakaya yerleþtir
+            {
+                ReplaceBlocks(SelectedBlock, avaibleSlotIndex);
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    #region seçilen bloðun yerleþmesi gereken indexi döndürür
+    //-1 dönerse ýstakada hiç yer yok demek. -1 dýþýnda gelen herhangi bir deðer seçilen bloðun konulmasý gereken indextir.
+    //Ancak bu indexte baþka blok olabilir. Varsa diðer bloklarýn kayrýlmasý gerekmektedir.
+    int GetAvaibleBlockSlotIndex(Transform selectedBlock)
     {
-        
-    }
+        int index = -1;
 
-    public int GetAvaibleBlockSlotIndex()
+        for (int i = 0; i < blockSlotsStruckList.Count; i++)
+        {
+            //Bir blok seçildiðinde, varsa ýstakada kendi cinsinden ilk bloðun indexini buluyoruz.
+            if (blockSlotsStruckList[i].snappedBlock != null && blockSlotsStruckList[i].snappedBlockType == selectedBlock.GetComponent<BlockProperties>().blockType)
+            {
+                if ((i + 1) < blockSlotsStruckList.Count)
+                {
+                    //ýstakada seçilen blok tipinden bir tane daha blok var ve eþleþtiði blokatan sonra baþka bir blok var.
+                    if (blockSlotsStruckList[i + 1].snappedBlock != null)
+                    {
+                        //ilk eþlemeden sonraki elaman yine kendi tipindemi kontrol ediliyor. Yani seçilen bloktan önce ýstakada kendi tipinden 2 elaman var mý kontrolü
+                        if (blockSlotsStruckList[i + 1].snappedBlockType == selectedBlock.GetComponent<BlockProperties>().blockType)
+                        {
+                            if ((i + 2) < blockSlotsStruckList.Count)//kendisinden önce 2 tane daha ayný tip blok vardýr.
+                            {
+                                index = i + 2;
+                                break;
+                            }
+                            else //ýstakada yer yoktur.
+                            {
+                                index = -1;
+                                break;
+                            }
+                        }
+                        else //ýstakada seçilen blok tipinden bir tane daha blok var. Ancak eþleþen bloktan sonra baþka tipde bloka var.
+                        {
+                            index = i + 1;
+                            break;
+                        }
+                    }
+                    else//ýstakada seçilen blok tipinden bir tane daha blok var ve sonuncu sýrada ayrýca ýstakada yer var.
+                    {
+                        index = i + 1;
+                        break;
+                    }
+                }
+                else
+                {
+                    //ýstakada yer yoktur. Eþleþtiði blok ýstakanýn sonuncu elemanýdýr.
+                    index = -1;
+                    break;
+                }
+            }
+            else if (blockSlotsStruckList[i].snappedBlock == null)//ýstakada bir sürü blok var ancak seçilen blok tipinden hiç blok yok ya da ýstaka tamamen boþ.
+            {
+                index = i;
+                break;
+            }
+        }
+
+        return index;
+    }
+    #endregion
+
+    //ýstakadaki ilk boþ slotu döndürür. -1 ise ýstakada tamamen doludur.
+    int GetAvaibleBlockSlotIndex()
     {
         int index = -1;
         for (int i = 0; i < BlockSlots.Count; i++)
@@ -41,11 +106,11 @@ public class BlockBoardController : MonoBehaviour
                 break;
             }
         }
-
         return index;
     }
 
-    public void FillBlockBoardStatus()
+    //ýstaka üzerindeki son durumu toplar
+    void FillBlockBoardStatus()
     {
         blockSlotsStruckList.Clear();
         for (int i = 0; i < BlockSlots.Count; i++)
@@ -55,7 +120,7 @@ public class BlockBoardController : MonoBehaviour
                 slot = BlockSlots[i],
                 index = i,
                 snappedBlock = BlockSlots[i].GetComponent<BlockSlotProperties>().snappedBlock ?? null,
-                snappedBlockType = BlockSlots[i].GetComponent<BlockSlotProperties>().snappedBlock == null ? -1: BlockSlots[i].GetComponent<BlockSlotProperties>().snappedBlock.GetComponent<BlockProperties>().blockType
+                snappedBlockType = BlockSlots[i].GetComponent<BlockSlotProperties>().snappedBlock == null ? -1 : BlockSlots[i].GetComponent<BlockSlotProperties>().snappedBlock.GetComponent<BlockProperties>().blockType
             };
             blockSlotsStruckList.Add(slot);
         }
@@ -63,31 +128,119 @@ public class BlockBoardController : MonoBehaviour
         BlockMatchControl();
     }
 
+    //3 tane ayný bloktan varsa patlatýr
     void BlockMatchControl()
     {
         for (int i = 0; i < blockSlotsStruckList.Count; i++)
         {
-            List<BlockSlotStruct> MatchedBlocks = blockSlotsStruckList.FindAll(x => x.snappedBlockType.Equals(blockSlotsStruckList[i].snappedBlockType));
-
-            if (MatchedBlocks.Count == 3)
+            if (blockSlotsStruckList[i].snappedBlock != null)
             {
-                foreach (BlockSlotStruct block in MatchedBlocks)
+                List<BlockSlotStruct> MatchedBlocks = blockSlotsStruckList.FindAll(x => x.snappedBlockType.Equals(blockSlotsStruckList[i].snappedBlockType));
+
+                if (MatchedBlocks.Count == 3)
                 {
-                    if (block.snappedBlock != null)
+                    foreach (BlockSlotStruct block in MatchedBlocks)
                     {
-                        Destroy(block.snappedBlock.gameObject);
+                        if (block.snappedBlock != null)
+                        {
+                            Destroy(block.snappedBlock.gameObject);
+                        }
+
+                        block.snappedBlock = null;
+                        block.snappedBlockType = -1;
                     }
-                    
-                    block.snappedBlock = null;
-                    block.snappedBlockType = -1;
+
+                    #region patlamasý gereken bloklar patladýktan sonra geriye kalan bloklar aralarýnda boþluk kalmayacak þekilde tekrar yerleþtirilir.
+
+                    MatchedBlocks.Sort((x, y) => x.index.CompareTo(y.index));
+
+                    for (int x = 0; x < MatchedBlocks.Count; x++)
+                    {
+                        for (int y = MatchedBlocks[x].index; y < blockSlotsStruckList.Count; y++)
+                        {
+                            if (blockSlotsStruckList[y].snappedBlock != null)
+                            {
+                                StartCoroutine(SmoothMoveToSnapPointSimple(blockSlotsStruckList[y].snappedBlock, blockSlotsStruckList[MatchedBlocks[x].index].slot, MatchedBlocks[x].index));
+                                
+                                //bulunan dolu slotu boþalan sloata atýyoruz.
+                                blockSlotsStruckList[MatchedBlocks[x].index].snappedBlock = blockSlotsStruckList[y].snappedBlock;
+                                blockSlotsStruckList[MatchedBlocks[x].index].snappedBlockType = blockSlotsStruckList[y].snappedBlockType;
+
+                                blockSlotsStruckList[y].snappedBlock = null;
+                                BlockSlots[y].GetComponent<BlockSlotProperties>().snappedBlock = null;
+                                blockSlotsStruckList[y].snappedBlockType = -1;
+                                break;
+                            }
+                        }
+                    }
+
+                    #endregion
+                    break;
                 }
             }
         }
     }
 
-    public void PlaceBlock()
+    //bir bloðu uygun yere smooth þekilde yerleþmesini saðlar
+    IEnumerator SmoothMoveToSnapPoint(Transform block, Transform snapPoint, int avaibleSlotIndex)
     {
+        isSmoothMoveToSnapPointAnimationContinue = true;
+        block.GetComponent<Rigidbody>().detectCollisions = false;
 
+        while (Vector3.Distance(block.position, snapPoint.position) > 0.05f)
+        {
+            block.position = Vector3.MoveTowards(block.position, snapPoint.position, Time.deltaTime * 5);
+            yield return null;
+        }
+        block.position = snapPoint.position;
+
+        //ýstakada yer alan slota bloðun kendisi setlernir.
+        transform.GetComponent<BlockBoardController>().BlockSlots[avaibleSlotIndex].transform.GetComponent<BlockSlotProperties>().snappedBlock = block;
+        block.GetComponent<BlockProperties>().IsSnapped = true;
+
+        //ýstaka üzerine yerleþtirilen blocklar kontrol edilir/patlatýlýr vs...
+        transform.GetComponent<BlockBoardController>().FillBlockBoardStatus();
+
+        //ýstaka üzerinde kalan boþ slotlar aranýr
+        avaibleSlotIndex = transform.GetComponent<BlockBoardController>().GetAvaibleBlockSlotIndex();
+        if (avaibleSlotIndex == -1)//-1 ise slotlar dolmuþtur game over olur.
+        {
+            Debug.Log("Game Over");
+        }
+        isSmoothMoveToSnapPointAnimationContinue = false;
     }
 
+    IEnumerator SmoothMoveToSnapPointSimple(Transform block, Transform snapPoint, int avaibleSlotIndex)
+    {
+        while (Vector3.Distance(block.position, snapPoint.position) > 0.05f)
+        {
+            block.position = Vector3.MoveTowards(block.position, snapPoint.position, Time.deltaTime * 5);
+            yield return null;
+        }
+        block.position = snapPoint.position;
+
+        //ýstakada yer alan slota bloðun kendisi setlernir.
+        transform.GetComponent<BlockBoardController>().BlockSlots[avaibleSlotIndex].transform.GetComponent<BlockSlotProperties>().snappedBlock = block;
+        block.GetComponent<BlockProperties>().IsSnapped = true;
+    }
+
+    //seçilen bloðun yerleþmesi gerken index bulunduktan sonra o bloðun uygun indexe yerleþmesini saðlar. Gerekiyorsa diðer bloklarý birer kaydýrýr.
+    void ReplaceBlocks(Transform SelectedBlock, int avaibleSlotIndex)
+    {
+        if (blockSlotsStruckList[avaibleSlotIndex].snappedBlock != null) //seçilen bloðun yerleþmesi gereken indexte baþka blok vardýr.
+        {
+            for (int i = avaibleSlotIndex; i < blockSlotsStruckList.Count; i++)
+            {
+                if (blockSlotsStruckList[i].snappedBlock != null)
+                {
+                    StartCoroutine(SmoothMoveToSnapPointSimple(blockSlotsStruckList[i].snappedBlock, blockSlotsStruckList[i + 1].slot, i + 1));
+                }
+            }
+            StartCoroutine(SmoothMoveToSnapPoint(SelectedBlock, BlockSlots[avaibleSlotIndex], avaibleSlotIndex));
+        }
+        else // seçilen bloðun yerleþmesi gereken index boþtur.
+        {
+            StartCoroutine(SmoothMoveToSnapPoint(SelectedBlock, BlockSlots[avaibleSlotIndex], avaibleSlotIndex));
+        }
+    }
 }
--- Assets/Code/BlockTouchController.cs
+++ Assets/Code/BlockTouchController.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class BlockTouchController : MonoBehaviour
 {
-    bool isSmoothMoveToSnapPointAnimationContinue = false;
+    //bool isSmoothMoveToSnapPointAnimationContinue = false;
 
     // Start is called before the first frame update
     void Start()
@@ -37,46 +37,47 @@ public class BlockTouchController : MonoBehaviour
                 if (raycastHit.collider.gameObject.tag == GlobalVariables.TagBlock
                     && raycastHit.collider.gameObject.GetComponent<BlockProperties>().IsThereAnyBlockOnIt == false
                     && raycastHit.collider.gameObject.GetComponent<BlockProperties>().IsSnapped == false
-                    && isSmoothMoveToSnapPointAnimationContinue == false
                     )
                 {
-                    int avaibleSlotIndex = transform.GetComponent<BlockBoardController>().GetAvaibleBlockSlotIndex();
-                    if (avaibleSlotIndex != -1) //bloklarý ýstakaya yerleþtir
-                    {
-                        //seçilen blok ýskaya yerleþtirilir.
-                        StartCoroutine(SmoothMoveToSnapPoint(raycastHit.collider.transform, transform.GetComponent<BlockBoardController>().BlockSlots[avaibleSlotIndex], avaibleSlotIndex));
-                    }
+
+                    transform.GetComponent<BlockBoardController>().PlaceBlock(raycastHit.collider.transform);
+                    //int avaibleSlotIndex = transform.GetComponent<BlockBoardController>().GetAvaibleBlockSlotIndex();
+                    //if (avaibleSlotIndex != -1) //bloklarý ýstakaya yerleþtir
+                    //{
+                    //    //seçilen blok ýskaya yerleþtirilir.
+                    //    //StartCoroutine(SmoothMoveToSnapPoint(raycastHit.collider.transform, transform.GetComponent<BlockBoardController>().BlockSlots[avaibleSlotIndex], avaibleSlotIndex));
+                    //}
                 }
             }
         }
     }
     #endregion
 
-    IEnumerator SmoothMoveToSnapPoint(Transform block, Transform snapPoint, int avaibleSlotIndex)
-    {
-        isSmoothMoveToSnapPointAnimationContinue = true;
-        block.GetComponent<Rigidbody>().detectCollisions = false;
+    //IEnumerator SmoothMoveToSnapPoint(Transform block, Transform snapPoint, int avaibleSlotIndex)
+    //{
+    //    isSmoothMoveToSnapPointAnimationContinue = true;
+    //    block.GetComponent<Rigidbody>().detectCollisions = false;
 
-        while (Vector3.Distance(block.position, snapPoint.position) > 0.05f)
-        {
-            block.position = Vector3.MoveTowards(block.position, snapPoint.position, Time.deltaTime * 5);
-            yield return null;
-        }
-        block.position = snapPoint.position;
+    //    while (Vector3.Distance(block.position, snapPoint.position) > 0.05f)
+    //    {
+    //        block.position = Vector3.MoveTowards(block.position, snapPoint.position, Time.deltaTime * 5);
+    //        yield return null;
+    //    }
+    //    block.position = snapPoint.position;
 
-        //ýstakada yer alan slota bloðun kendisi setlernir.
-        transform.GetComponent<BlockBoardController>().BlockSlots[avaibleSlotIndex].transform.GetComponent<BlockSlotProperties>().snappedBlock = block;
-        block.GetComponent<BlockProperties>().IsSnapped = true;
+    //    //ýstakada yer alan slota bloðun kendisi setlernir.
+    //    transform.GetComponent<BlockBoardController>().BlockSlots[avaibleSlotIndex].transform.GetComponent<BlockSlotProperties>().snappedBlock = block;
+    //    block.GetComponent<BlockProperties>().IsSnapped = true;
 
-        //ýstaka üzerine yerleþtirilen blocklar kontrol edilir/patlatýlýr vs...
-        transform.GetComponent<BlockBoardController>().FillBlockBoardStatus();
+    //    //ýstaka üzerine yerleþtirilen blocklar kontrol edilir/patlatýlýr vs...
+    //    transform.GetComponent<BlockBoardController>().FillBlockBoardStatus();
 
-        //ýstaka üzerinde kalan boþ slotlar aranýr
-        avaibleSlotIndex = transform.GetComponent<BlockBoardController>().GetAvaibleBlockSlotIndex();
-        if (avaibleSlotIndex == -1)//-1 ise slotlar dolmuþtur game over olur.
-        {
-            Debug.Log("Game Over");
-        }
-        isSmoothMoveToSnapPointAnimationContinue = false;
-    }
+    //    //ýstaka üzerinde kalan boþ slotlar aranýr
+    //    avaibleSlotIndex = transform.GetComponent<BlockBoardController>().GetAvaibleBlockSlotIndex();
+    //    if (avaibleSlotIndex == -1)//-1 ise slotlar dolmuþtur game over olur.
+    //    {
+    //        Debug.Log("Game Over");
+    //    }
+    //    isSmoothMoveToSnapPointAnimationContinue = false;
+    //}
 }
